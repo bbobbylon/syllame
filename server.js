7679d3fb38947e5c3eac127c689239ef7e7140d8
bbobbylon <robertcoliver@lewisu.edu>
2021-01-03 10:08:15 -1000

    works locally

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const express = require ('express'); //node.js flexible web application framewor
 const mongoose = require ("mongoose"); // Mongoose is a MongoDB object modeling toold designed to work in an asynchronous environment, supporting both promises and callbacks
 const bodyParser = require ("body-parser"); //node.js body parsing middleware
 const passport = require("passport"); //used to authenticate requests, via plugins known as  ~strategies~.
+const path = require("path");
 
 const users = require("./routes/api/users");
 
@@ -45,7 +46,20 @@ app.use(passport.initialize());
 require("./config/passport")(passport);
 
 //our routes
-app.use("/api/users", users)
+app.use("/api/users", users);
+
+//serve our static assets (build folder if we are in production)
+
+if(process.env.NODE_ENV=== 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build' , 'index.html'));
+
+    })
+}
 
 
     const port = process.env.PORT || 5000; //process.env.PORT is heroku's port if you choose to deploy the app
